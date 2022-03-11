72d74343509a2be7fabd24202ebf262a97799f9e
Allleksandr4444 <allleksandr4444@mail.ru>
2022-03-11 13:45:18 +0300

    Решение task2.

---
+++ task2/solution_task2.py
@@ -0,0 +1,33 @@
+def point_position(file1, file2):
+    points = []
+    with open(file1, 'r') as file_1:
+        radius_centr = file_1.read().split('\n')
+    with open(file2, 'r') as file_2:
+        while True:
+            line = file_2.readline().strip()
+            if line:
+                points.append(line.split('\n'))
+            if not line:
+                break
+
+    points = [[float(i) for i in j[0].split(' ')] for j in points]
+    centr_radius = [[float(i) for i in j.split(' ')] for j in radius_centr]
+    radius = centr_radius[1][0]
+    centr_x = centr_radius[0][0]
+    centr_y = centr_radius[0][1]
+
+    for point in points:
+        x = point[0]
+        y = point[1]
+        result = ((x - centr_x) ** 2 + (y - centr_y) ** 2) ** 0.5
+        if result < radius:
+            return 1  # точка лежит внутри
+        elif result > radius:
+            return 2  # точка лежит снаружи
+        elif result == radius:
+            return 0  # точка лежит на окружности
+
+
+
+if __name__ == "__main__":
+    print(point_position('file1.txt', 'file2.txt'))
